2e7872557fce7310fc3d5e77cc58f02722665d5c
a.kudryavtsev <a.kudryavtsev@artox.com>
2021-03-16 21:54:28 +0300

    feat: add functionality

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const img = document.getElementById('img');
     const canvas = document.getElementById('canvas');
     const downloadButton = document.getElementById('downloadButton');
+    const context = canvas.getContext('2d');
 
     img.addEventListener('load', () => {
-        // canvas.height = img.height;
-        // canvas.width = img.width;
-        const context = canvas.getContext('2d');
+        canvas.height = img.height;
+        canvas.width = img.width;
         context.filter = 'contrast(1.4) sepia(1) blur(3)';
         context.drawImage(img, 0, 0, img.width * 1, img.height * 1);
     });
