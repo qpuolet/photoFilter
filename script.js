d478bbfb23cb2c2cb251a1a3de6a272f5e3c2675
a.kudryavtsev <a.kudryavtsev@artox.com>
2021-03-16 21:44:54 +0300

    feat: add functionality

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const img = document.getElementById('img');
+    const canvas = document.getElementById('canvas');
+    const downloadButton = document.getElementById('downloadButton');
+
+    img.addEventListener('load', () => {
+        const context = canvas.getContext('2d');
+        context.filter = 'contrast(1.4) sepia(1)';
+        context.drawImage(img, 0, 0, img.width * 1, img.height * 1);
+    });
+
+    downloadButton.addEventListener('click', () => {
+        // get canvas data
+        const image = canvas.toDataURL();
+
+        // create temporary link
+        const tmpLink = document.createElement('a');
+        tmpLink.download = 'filteredImage.png'; // set the name of the download file
+        tmpLink.href = image;
+
+        // temporarily add link to body and initiate the download
+        document.body.appendChild(tmpLink);
+        tmpLink.click();
+        document.body.removeChild(tmpLink);
+    });
+});
